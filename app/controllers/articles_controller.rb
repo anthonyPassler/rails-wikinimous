bdc1674385ac220dd20dc3c9eceb7095edc55744
Anthony Passler <passleranthony@yahoo.com>
2020-05-12 16:09:42 +0100

    added edit, update and destroymethods in the controller and created all viws

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,22 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
